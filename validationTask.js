f27c30408368ba4437e36b00f6ee9dc5894b2a99
Andrew <manloengchung@yahoo.co.uk>
2019-07-23 18:25:30 +0100

    refactored our code

--- validationTask.js
+++ validationTask.js
@@ -4,29 +4,42 @@ const usernameElement = document.getElementById('username');
 const passwordElement = document.getElementById('password');
 
 function validateInput() {
-	console.dir(this.id)
+	//refactored the reg obj to take reg and msg
 	const refObj = {
-		username: /[^a-z0-9\s-']/,
-		firstname: /[^a-z]+/i,
-		lastname: /[^a-z]+/i
+		username: {
+			reg: /[^a-z0-9\s-']/,
+			msg: 'Incorrect username input'
+		},
+		firstname: {
+			reg: /[^a-z]+/i,
+			msg: 'Incorrect firstname input'
+		},
+		lastname: {
+			reg: /[^a-z]+/i,
+			msg: 'Incorrect lastname input'
+		}
+	};
 
-	}
-	console.log(!refObj[this.id])
-	if (!refObj[this.id].test(this.value)) {
+	//refactored our check - refObj[this.id].reg.test(this.value) to read regex
+	if (!refObj[this.id].reg.test(this.value)) {
 		this.classList.remove('invalid');
 		this.classList.add('valid');
-
 	} else {
 		this.classList.remove('valid');
 		this.classList.add('invalid');
-		const errMsg = document.createElement('p')
-		errMsg.innerText = "Incorrect input"
-		this.parentNode[0].append(errMsg)
-		this.parentNode.appendChild(errMsg)
+		const errMsg = document.createElement('p');
 
-		// console.dir(this.parentNode.appendChild(errMsg))
+		// refactored 	errMsg.innerText = refObj[this.id] to read our newly created msg
+		errMsg.innerText = refObj[this.id].msg;
+		this.parentNode.append(errMsg);
+
+		// not needed anymore - was our previous test
+		// // this.parentNode.appendChild(errMsg);
+		// console.log(this.parentNode);
+
+		// // console.dir(this.parentNode.appendChild(errMsg))
 	}
-};
+}
 
 inputElements.forEach((element) => {
 	element.addEventListener('change', validateInput);
