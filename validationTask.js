81688fdfc97f33def65e16a042ee07c3026ec6a9
Achugs <anna_9400@hotmail.com>
2019-07-23 16:49:54 +0100

    form half complete and some styling complete

--- validationTask.js
+++ validationTask.js
@@ -3,14 +3,28 @@ const inputElements = document.querySelectorAll('input');
 const usernameElement = document.getElementById('username');
 const passwordElement = document.getElementById('password');
 
-const validateInput = (event) => {
-	const reg = /\d+/g;
-	if (reg.test(usernameElement.value)) {
-		usernameElement.classList.remove('invalid');
-		usernameElement.classList.add('valid');
+function validateInput() {
+	console.dir(this.id)
+	const refObj = {
+		username: /[^a-z0-9\s-']/,
+		firstname: /[^a-z]+/i,
+		lastname: /[^a-z]+/i
+
+	}
+	console.log(!refObj[this.id])
+	if (!refObj[this.id].test(this.value)) {
+		this.classList.remove('invalid');
+		this.classList.add('valid');
+
 	} else {
-		usernameElement.classList.remove('valid');
-		usernameElement.classList.add('invalid');
+		this.classList.remove('valid');
+		this.classList.add('invalid');
+		const errMsg = document.createElement('p')
+		errMsg.innerText = "Incorrect input"
+		this.parentNode[0].append(errMsg)
+		this.parentNode.appendChild(errMsg)
+
+		// console.dir(this.parentNode.appendChild(errMsg))
 	}
 };
 
